0f2b0362d5cd90ee9b9b0e6bac5043bb96bdd6b4
mohdfardeen7007 <162273323+mohdfardeen7007@users.noreply.github.com>
2026-03-11 12:01:56 +0530

    Update bot settings with new name and owner number

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: '𝐅𝖾ᥣ𝗂𝗑',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "𝐅𝖾ᥣ𝗂𝗑",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '918127875972', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
